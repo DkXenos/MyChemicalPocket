b0fca8a7d182515bb95d1363a4ee5d3102b88651
DkXenos <superjason101010@gmail.com>
2026-04-27 08:45:43 +0700

    xcode init

---
+++ Chem.io/Chem_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chem_ioApp.swift
+//  Chem.io
+//
+//  Created by Jason TIo on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Chem_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
